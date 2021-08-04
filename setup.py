5224fdb2fea5697833ee785a62f6653730f47d22
Chris <cbladwell@gmail.com>
2021-08-04 14:22:42 +1000

    init commit of boilerplate

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='ohw-proj-bioaccoustics',
+    url='https://github.com/oceanhackweek/owh-proj-bioacoustics',
+    author='Nick Mortimer, Chris Bladwell', # others
+    author_email='',
+    # Packages included
+    packages=['bioacoustics'],
+    # dependencies
+    install_requires=['numpy', 'xarray', 'intake', ],
+    version='0.1',
+    license='MIT',
+    description='A package of tools to import and analyse bioacoustics data',
+)
